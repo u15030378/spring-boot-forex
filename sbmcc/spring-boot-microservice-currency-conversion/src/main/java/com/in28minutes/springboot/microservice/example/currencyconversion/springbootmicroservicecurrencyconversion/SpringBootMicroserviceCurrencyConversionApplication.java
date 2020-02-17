9f1ff255aceb98879e75edc6c882eaa9f1650d8b
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-17 16:29:31 +0200

    Part2 fixing continues

--- sbmcc/spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
+++ sbmcc/spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -2,8 +2,12 @@ package com.in28minutes.springboot.microservice.example.currencyconversion.sprin
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicecurrencyconversion")
+@EnableDiscoveryClient
 public class SpringBootMicroserviceCurrencyConversionApplication {
 
 	public static void main(String[] args) {
