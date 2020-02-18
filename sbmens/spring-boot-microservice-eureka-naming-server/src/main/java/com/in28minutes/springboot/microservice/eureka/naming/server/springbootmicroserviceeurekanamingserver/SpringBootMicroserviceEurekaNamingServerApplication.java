2c702ae0ca745afaf08875b66bae1b6f1ac454f3
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-18 10:21:23 +0200

    Eureka service added

---
+++ sbmens/spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ sbmens/spring-boot-microservice-eureka-naming-server/src/test/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.microservice.eureka.naming.server.springbootmicroserviceeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
