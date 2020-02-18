b3feb09481ba00da0b2020e8c3e5431f676a3547
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-18 11:50:32 +0200

    Part 4 complete

--- sbmens/spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplication.java
+++ sbmens/spring-boot-microservice-eureka-naming-server/src/main/java/com/in28minutes/springboot/microservice/eureka/naming/server/springbootmicroserviceeurekanamingserver/SpringBootMicroserviceEurekaNamingServerApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.springboot.microservice.eureka.naming.server.springbootm
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringBootMicroserviceEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
