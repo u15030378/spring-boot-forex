75d472adde7a76573d90c2ec2719bf8bc2f1d0c9
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-18 10:00:44 +0200

    Part 3 complete

--- sbmcc/spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
+++ sbmcc/spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -5,7 +5,9 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="forex-service", url="localhost:8000")
+//@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue
