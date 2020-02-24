d21cc02ebc246c1b35fa506a9f6c12fa33ee26af
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-24 10:24:14 +0200

    There is no Dana, only Zuul

---
+++ sbmz/spring-boot-microservice-zuul/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicezuul/SpringBootMicroserviceZuulApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceZuulApplication.class, args);
+	}
+
+}
---
+++ sbmz/spring-boot-microservice-zuul/src/test/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicezuul/SpringBootMicroserviceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
