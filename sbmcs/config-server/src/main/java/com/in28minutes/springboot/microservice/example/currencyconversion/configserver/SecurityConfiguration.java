77caeb218c18bd5906130e41d60026426d6106a1
TransvaalseSeePloeg <u15030378@tuks.co.za>
2020-02-25 10:20:31 +0200

    Config server done. Does a useless user.role config for zuul

---
+++ sbmcs/config-server/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/configserver/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion.configserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringAntMatchers("/encrypt/**")
+                .ignoringAntMatchers("/decrypt/**");
+
+        super.configure(http);
+    }
+}
--- sbmz/spring-boot-microservice-zuul/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicezuul/SpringBootMicroserviceZuulApplication.java
+++ sbmz/spring-boot-microservice-zuul/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicezuul/SpringBootMicroserviceZuulApplication.java
@@ -1,17 +1,33 @@
 package com.in28minutes.springboot.microservice.example.currencyconversion.springbootmicroservicezuul;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@RefreshScope
 public class SpringBootMicroserviceZuulApplication {
+
+	@Value("${user.role}")
+	private String role;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceZuulApplication.class, args);
+	}
+
+	@GetMapping(
+		value="whoami/{username}",
+		produces= MediaType.TEXT_PLAIN_VALUE)
+	public String whoami(@PathVariable("username") String username) {
+		return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
 	}
 
 }
